9319dd2f2cb26fadb1a3206e7b02262c1fc8707c
Diego Sotorivero <123613958+DSr2111@users.noreply.github.com>
2024-08-19 07:53:21 -0400

    wip

--- eightball/src/EightBall.js
+++ eightball/src/EightBall.js
@@ -1,2 +1,6 @@
 import React, { useState } from "react";
 import "./css/EightBall";
+
+function EightBall({}) {
+  return <div className="EightBall" onClick={handleClick} style={{backgroundColor: }}></div>;
+}
