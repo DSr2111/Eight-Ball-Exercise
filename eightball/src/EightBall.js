7322d8ac86bc17290ee43137295ef0cdbfa1b9cf
Diego Sotorivero <123613958+DSr2111@users.noreply.github.com>
2024-08-19 07:57:33 -0400

    WIP, imports and more

--- eightball/src/EightBall.js
+++ eightball/src/EightBall.js
@@ -1,10 +1,19 @@
 import React, { useState } from "react";
 import "./css/EightBall";
+import { choice } from "./random";
+import defaultAnswers from "./answers.json";
 
 function EightBall({}) {
-
-    function handleClick(evt) {
-        setAnswer(choice(answers));
-    }
-  return <div className="EightBall" onClick={handleClick} style={{backgroundColor: }}></div>;
+  function handleClick(evt) {
+    setAnswer(choice(answers));
+  }
+  return (
+    <div
+      className="EightBall"
+      onClick={handleClick}
+      style={{ backgroundColor: answer.color }}
+    ></div>
+  );
 }
+
+export default EightBall;
