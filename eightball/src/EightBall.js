3975215782c310906eefc1cb506156f3bc561de0
Diego Sotorivero <123613958+DSr2111@users.noreply.github.com>
2024-08-19 07:54:40 -0400

    handleClick add

--- eightball/src/EightBall.js
+++ eightball/src/EightBall.js
@@ -2,5 +2,9 @@ import React, { useState } from "react";
 import "./css/EightBall";
 
 function EightBall({}) {
+
+    function handleClick(evt) {
+        setAnswer(choice(answers));
+    }
   return <div className="EightBall" onClick={handleClick} style={{backgroundColor: }}></div>;
 }
